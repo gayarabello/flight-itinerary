07fb35d4b9ca1a077df38663910b201aa5836dfd
gayarabello <gayarabello@gmail.com>
2022-10-07 00:14:04 +0100

    Create test data

---
+++ main.js
@@ -0,0 +1,22 @@
+const onewayTickets = [
+    ['ES', 'JAM'],
+    ['BRA', 'EUA'],
+    ['AUS', 'GER'],
+    ['PT', 'JAP'],
+    ['JAM', 'AUS'],
+    ['GER', 'SWE'],
+    ['EUA', 'PT'],
+    ['JAP', 'ES'],
+]
+
+/* const testSolution = [
+    ['BRA', 'EUA'],
+    ['EUA', 'PT'],
+    ['PT', 'JAP'],
+    ['JAP', 'ES'],
+    ['ES', 'JAM'],
+    ['JAM', 'AUS'],
+    ['AUS', 'GER'],
+    ['GER', 'SWE'],
+]
+ */
